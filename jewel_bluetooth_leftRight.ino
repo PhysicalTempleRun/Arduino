28d66a4f44a44ba942a9af0341aaa2a9b57e32ba
mskwon02 <121784261+mskwon02@users.noreply.github.com>
2023-12-09 09:19:54 +0900

    보석 코드

---
+++ jewel_bluetooth_leftRight.ino
@@ -0,0 +1,93 @@
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+const int MPU_ADDR = 0x68; // I2C통신을 위한 MPU6050의 주소
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ; // 가속도(Acceleration)와 자이로(Gyro)
+double angleAcX, angleAcY, angleAcZ;
+const double RADIAN_TO_DEGREE = 180 / 3.14159;
+
+// HC-06 블루투스 모듈을 위한 핀 설정
+const int BT_RX = 2; // HC-06 RX 핀
+const int BT_TX = 3; // HC-06 TX 핀
+SoftwareSerial bluetooth(BT_RX, BT_TX); // RX, TX
+
+void setup() {
+  initSensor();
+  Serial.begin(9600); // 시리얼 통신 속도 설정 
+  bluetooth.begin(9600); // 블루투스 통신 속도 설정 
+}
+
+void loop() {
+  getData();
+
+  angleAcX = atan(AcY / sqrt(pow(AcX, 2) + pow(AcZ, 2))) * RADIAN_TO_DEGREE;
+  angleAcY = atan(-AcX / sqrt(pow(AcY, 2) + pow(AcZ, 2))) * RADIAN_TO_DEGREE;
+
+  checkAndPrintAngle();
+  delay(100);
+}
+
+void initSensor() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); 
+  Wire.write(0x6B);    
+  Wire.write(0);
+  Wire.endTransmission(true);
+}
+
+void getData() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);   
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 14, true);  
+  AcX = Wire.read() << 8 | Wire.read(); 
+  AcY = Wire.read() << 8 | Wire.read();
+  AcZ = Wire.read() << 8 | Wire.read();
+  Tmp = Wire.read() << 8 | Wire.read();
+  GyX = Wire.read() << 8 | Wire.read();
+  GyY = Wire.read() << 8 | Wire.read();
+  GyZ = Wire.read() << 8 | Wire.read();
+}
+
+void checkAndPrintAngle() {
+  if (angleAcX >= 50) {
+
+    //0.1초 있다가 다시 값 확인해봐서 그때도 기울이고 있는지 확인
+    delay(100);
+    getData();
+    double recheckAngleAcX = atan(AcY / sqrt(pow(AcX, 2) + pow(AcZ, 2))) * RADIAN_TO_DEGREE;
+    
+    if (recheckAngleAcX >= 50) {
+      //mac과 블루투스 통신시
+      Serial.println("left");
+
+      //윈도우와 블루투스 통신시
+      //bluetooth.println("left");
+      //안되면 아래걸로 해보기
+			//bluetooth.write("left"); // 숫자를 문자로 변환하여 연결된 곳에 전송
+	    //bluetooth.write('\r'); // 캐리지 리턴 전송
+	    //bluetooth.write('\n'); // 라인 피드 전송
+			
+      delay(1500); //기울였다가 다시 제자리로 돌아가는 동안에 읽히는 값은 무시하기위함
+    }
+  } else if (angleAcX <= -50) {
+
+    //0.1초 있다가 다시 값 확인해봐서 그때도 기울이고 있는지 확인
+    delay(100);
+    getData();
+    double recheckAngleAcX = atan(AcY / sqrt(pow(AcX, 2) + pow(AcZ, 2))) * RADIAN_TO_DEGREE;
+
+    if (recheckAngleAcX <= -50) {
+      //mac과 블루투스 통신시
+      Serial.println("right");
+
+      //윈도우와 블루투스 통신시
+      //bluetooth.println("right");
+      //안되면 아래걸로 해보기
+			//bluetooth.write("right"); // 숫자를 문자로 변환하여 연결된 곳에 전송
+	    //bluetooth.write('\r'); // 캐리지 리턴 전송
+	    //bluetooth.write('\n'); // 라인 피드 전송
+      delay(1500); //기울였다가 다시 제자리로 돌아가는 동안에 읽히는 값은 무시하기위함
+    }
+  }
+}
